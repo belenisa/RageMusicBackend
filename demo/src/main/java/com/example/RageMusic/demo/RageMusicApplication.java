5a2688e3b9d5856116aa836599184a93830e6530
belenisa <bel.alarconh@duocuc.cl>
2025-11-06 08:56:25 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/RageMusic/demo/RageMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.RageMusic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RageMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RageMusicApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/RageMusic/demo/RageMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RageMusic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RageMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
