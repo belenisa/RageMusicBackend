adfe120b49a47e26ff50ca5d79d318447af310c2
belenisa <bel.alarconh@duocuc.cl>
2025-11-12 20:10:13 -0300

    Swagger

---
+++ ragemusica_Backend/src/main/java/com/example/ragemusica/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.ragemusica.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+
+   @Bean
+   public OpenAPI customOpenAPI(){
+       return new OpenAPI().info(
+           new Info()
+           .title("API RageMusic")
+           .version("0.1")
+           .description("Api de repaso")
+       );
+   }
+
+
+}
