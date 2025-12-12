672994990c50381e0a25b99c81d3e07580b76083
belenisa <bel.alarconh@duocuc.cl>
2025-12-11 23:30:47 -0300

    23

--- src/main/java/com/example/ragemusica/model/Imagenes.java
+++ src/main/java/com/example/ragemusica/model/Imagenes.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,7 +30,7 @@ public class Imagenes {
     private String url;
 
 
-    @ManyToMany
+    @OneToOne
     @JoinColumn(name = "producto_id")
     private Producto producto;
 
